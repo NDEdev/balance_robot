bb81ab9bafb508e608dc1f2a799c73d8fb427d46
dNdt <demidnau@gmail.com>
2018-04-15 10:40:43 +0700

    отладил sysclock config, проблема с зависанием vTaskDelay осталась

--- cfg/FreeRTOSConfig.h
+++ cfg/FreeRTOSConfig.h
@@ -97,7 +97,7 @@
 #define configSUPPORT_DYNAMIC_ALLOCATION         0
 #define configUSE_IDLE_HOOK                      1
 #define configUSE_TICK_HOOK                      1
-#define configCPU_CLOCK_HZ                       ( SystemCoreClock )
+#define configCPU_CLOCK_HZ                       ( 168000000 )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 7 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
